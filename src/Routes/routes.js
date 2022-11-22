7c6b2461f87ede070dd62ebe448d53a1321530f3
LDAP\dvdat2 <vandat.k21@gmail.com>
2022-11-22 15:36:05 +0700

    feat: get detail user

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -4,7 +4,10 @@ import homeController from "../controller/homeController";
 const router = express.Router();
 
 const initRoutes = (app) => {
-  router.get("/", (req, res) => homeController(req, res));
+  router.get("/", (req, res) => homeController.getUsers(req, res));
+  router.get("/detail/user/:id", (req, res) =>
+    homeController.getUserDetail(req, res)
+  );
 
   router.get("/about", (req, res) => {
     res.send("About page");
--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,22 @@
 import connectionPoll from "../configs/connectDB";
-export default async function homeController(req, res) {
-  const [rows, fields] = await connectionPoll.execute("SELECT * FROM `users`");
+
+async function getUsers(req, res) {
+  const [rows, fields] = await connectionPoll.execute("SELECT * FROM users");
   return res.render("home/index.ejs", { dataUsers: rows });
 }
+
+async function getUserDetail(req, res) {
+  const id = req.params.id;
+  const [rows, fields] = await connectionPoll.execute(
+    `SELECT * FROM users where id=?`,
+    [id]
+  );
+  return res.render("userDetail/index.ejs", { detailUser: rows[0] });
+}
+
+const homeController = {
+  getUsers,
+  getUserDetail,
+};
+
+export default homeController;
